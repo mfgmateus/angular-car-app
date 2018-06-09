cafe7f22aa846b0538abdcd006e825b148acbc78
Mateus Ferreira <mateus.ferreira@projetas.com.br>
2018-06-09 14:20:26 -0300

    Retrying another version of karma-conf

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -34,13 +34,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadlessNoSandbox'],
-    customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    },
+    browsers: ['ChromiumNoSandbox'],
+	  customLaunchers: {
+		ChromiumNoSandbox: {
+		  base: 'ChromiumHeadless',
+		  flags: ['--headless', '--disable-gpu', '--disable-translate', '--disable-extensions']
+		}
+	},
     singleRun: false
   });
 };
